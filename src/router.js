500412829c197d41e52068cde17544ec0b395758
miqueiasbelfort <kawasousa1554@gmail.com>
2022-07-19 15:26:51 -0300

    first commit

---
+++ src/router.js
@@ -0,0 +1,30 @@
+import React from "react"
+import {NavigationContainer} from "@react-navigation/native"
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+
+// pages
+import Home from "./pages/Home"
+import Detail from "./pages/Detail"
+
+const Stack = createNativeStackNavigator()
+
+function Routes(){
+    return (
+        <NavigationContainer>
+
+            <Stack.Navigator>
+                <Stack.Screen 
+                    name="Home"
+                    component={Home}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen 
+                    name="Detail"
+                    component={Detail}
+                />
+            </Stack.Navigator>
+
+        </NavigationContainer>
+    )
+}
+export default Routes
